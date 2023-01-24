bb37b02ae3986dd8b32693b777851f7c5162cfc5
Sheyla De los Santos <delossantosshey@gmail.com>
2023-01-23 20:06:01 -0500

    Navbar redesign

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleMenu = () => {
+    setOpen(!open);
+  };
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <div>
-        <Link to="/lisaquest">Lisa Quest</Link>
-      </div>
+    <nav className="dropdown">
+      <button className="circle" onClick={handleMenu}></button>
+      {open ? (
+        <ul className="menu">
+          <li className="home-button">
+            <button>
+              <Link to={'/'} onClick={handleMenu}>
+                HOME
+              </Link>
+            </button>
+          </li>
+          <li className="projects-button">
+            <button>
+              <Link to={'/projects'} onClick={handleMenu}>
+                PROJECTS
+              </Link>
+            </button>
+          </li>
+        </ul>
+      ) : null}
     </nav>
   );
 };
