737d25de033646e9199b8875cab081ac9d996295
Sheyla De los Santos <delossantosshey@gmail.com>
2023-01-23 20:04:55 -0500

    Routes and home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import stars from '../../public/stars.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>hello world!</div>;
+  return (
+    <section>
+      <ul id="projects-images">
+        <li>
+          <Link to={'/projects/lisaQuest'}>
+            <img src="stars.png" className="image-project-stars"></img>
+          </Link>
+        </li>
+        <li>
+          <Link to={'/projects/allMatcha'}>
+            <img src="matcha.png" className="image-project"></img>
+          </Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Home;
--- client/components/Main.js
+++ client/components/Main.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Navbar from './Navbar';
 import LisaQuest from './LisaQuest';
+import Projects from './Projects';
 
 class Main extends Component {
   render() {
@@ -11,7 +12,9 @@ class Main extends Component {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/lisaquest" element={<LisaQuest />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/lisaQuest" element={<LisaQuest />} />
+          <Route path="projects/allMatcha" element={<LisaQuest />} />
         </Routes>
       </section>
     );
