efd338444091df03c80d242775645fba721e806e
Sheyla De los Santos <delossantosshey@gmail.com>
2023-01-23 20:06:51 -0500

    Added stylesheet

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './components/Main';
+import '../public/style.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
