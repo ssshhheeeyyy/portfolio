c0e869880100ae5a09e1330288bd133103a51034
Sheyla De los Santos <delossantosshey@gmail.com>
2023-01-25 22:13:15 -0500

    Added footer

---
+++ client/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://www.linkedin.com/in/sheyladelossantos/">linkedin</a>
+      <a href="https://github.com/ssshhheeeyyy">github</a>
+      <a href="mailto:delossantosshey@gmail.com">contact</a>
+    </footer>
+  );
+};
+
+export default Footer;
