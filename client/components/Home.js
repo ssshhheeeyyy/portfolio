c5d2ecab44de2e4cbb432f53c25bfeab0a964e7a
Sheyla De los Santos <delossantosshey@gmail.com>
2023-01-30 18:00:16 -0500

    cleaned up home

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,13 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <section className="home-intro">
-      <p
-        className="home-paragraph"
-        onMouseMove={(evt) => (evt.target.className = 'hovered')}
-        onMouseLeave={(evt) => (evt.target.className = 'home-paragraph')}
-      >
-        CLICK ON THE TRIANGLE TO LEARN MORE.
-      </p>
+      <p className="home-paragraph">CLICK ON THE TRIANGLE TO LEARN MORE.</p>
     </section>
   );
 };
