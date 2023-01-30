098f623e730d90771472a516d739ec65d696fa31
Sheyla De los Santos <delossantosshey@gmail.com>
2023-01-30 18:52:36 -0500

    changes to projects page

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Projects = () => {
+  const [desc, showDesc] = useState('');
+
   return (
-    <section>
-      <div id="projects-images">
-        <div>
-          <Link to={'/projects/lisaQuest'}>
-            <img src="stars.png" className="image-project"></img>
-          </Link>
-        </div>
-        <div>
-          <Link to={'/projects/allMatcha'}>
-            <img src="matcha.png" className="image-project"></img>
-          </Link>
-        </div>
+    <section
+      id="projects-images"
+      onMouseOver={(evt) => {
+        showDesc(evt.target.id);
+      }}
+      onMouseOut={() => {
+        showDesc('');
+      }}
+    >
+      <div id="projects-lisa">
+        <Link to={'/projects/lisaQuest'}>
+          <img src="stars.png" className="image-project" id="lisa-container" />
+        </Link>
+        {desc === 'lisa-container' ? (
+          <div id="lisa">
+            <h2>LISA QUEST</h2>
+            <p>Classic arcade style game</p>
+          </div>
+        ) : null}
+      </div>
+      <div>
+        <Link to={'/projects/allMatcha'}>
+          <img
+            src="matcha.png"
+            className="image-project"
+            id="matcha-container"
+          />
+        </Link>
+        {desc === 'matcha-container' ? (
+          <div id="matcha">
+            <h2>ALL MATCHA</h2>
+            <p>Fictional e-commerce website</p>
+          </div>
+        ) : null}
+      </div>
+      <div>
+        <Link to={'/projects/allMatcha'}>
+          <img src="room.png" className="image-project" id="room-container" />
+        </Link>
+        {desc === 'room-container' ? (
+          <div id="room">
+            <h2>ROOM</h2>
+            <p>Experiment with Three.js</p>
+          </div>
+        ) : null}
       </div>
     </section>
   );
