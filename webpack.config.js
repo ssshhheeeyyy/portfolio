6c74d9e95f1f0524ada32f794b96434dc278415c
Sheyla De los Santos <delossantosshey@gmail.com>
2023-02-04 13:34:57 -0500

    Webpack and .gitignore updates

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
   mode: 'development',
   context: __dirname,
