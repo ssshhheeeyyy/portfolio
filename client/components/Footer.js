bf1c5ebaa61450457ff9e5022dfad251e312406d
Sheyla De los Santos <delossantosshey@gmail.com>
2023-02-03 19:40:46 -0500

    Update to footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,9 +3,15 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <a href="https://www.linkedin.com/in/sheyladelossantos/">linkedin</a>
-      <a href="https://github.com/ssshhheeeyyy">github</a>
-      <a href="mailto:delossantosshey@gmail.com">contact</a>
+      <a href="https://www.linkedin.com/in/sheyladelossantos/" target="_blank">
+        linkedin
+      </a>
+      <a href="https://github.com/ssshhheeeyyy" target="_blank">
+        github
+      </a>
+      <a href="mailto:delossantosshey@gmail.com" target="_blank">
+        contact
+      </a>
     </footer>
   );
 };
