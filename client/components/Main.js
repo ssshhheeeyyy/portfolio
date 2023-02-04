34b985e5cf043b3bb51fe5d80d40774ef85b44c8
Sheyla De los Santos <delossantosshey@gmail.com>
2023-02-04 12:55:51 -0500

    Updated Main component, integrating 3D shape and updating routes

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,32 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Canvas } from '@react-three/fiber';
+import { Link } from 'react-router-dom';
 import Home from './Home';
-import Navbar from './Navbar';
 import LisaQuest from './LisaQuest';
 import Projects from './Projects';
 import About from './About';
 import Footer from './Footer';
+import AllMatcha from './AllMatcha';
+import Room from './Room';
+import Shape from './Shape';
 
-class Main extends Component {
-  render() {
-    return (
-      <main id="container">
-        <section id="navbar">
-          <Navbar />
-        </section>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="projects/lisaQuest" element={<LisaQuest />} />
-          <Route path="projects/allMatcha" element={<LisaQuest />} />
-        </Routes>
-        <section id="footer">
-          <Footer />
-        </section>
-      </main>
-    );
-  }
-}
+const Main = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div id="container">
+      <section id="navbar" onClick={handleMenu}>
+        <Canvas id="canvas">
+          <ambientLight intensity={15} />
+          <directionalLight position={[-2, 5, 2]} intensity={3} />
+          <spotLight position={[100, 1000, 100]} intensity={3} />
+          <Shape />
+        </Canvas>
+        <nav className="dropdown">
+          {open ? (
+            <ul className="menu">
+              <li className="home-link">
+                <Link to={'/'} onClick={handleMenu}>
+                  HOME
+                </Link>
+              </li>
+              <li className="projects-link">
+                <Link to={'/projects'} onClick={handleMenu}>
+                  PROJECTS
+                </Link>
+              </li>
+              <li className="about-link">
+                <Link to={'/about'} onClick={handleMenu}>
+                  ABOUT
+                </Link>
+              </li>
+            </ul>
+          ) : null}
+        </nav>
+      </section>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="projects">
+          <Route index element={<Projects />} />
+          <Route path="lisa-quest" element={<LisaQuest />} />
+          <Route path="all-matcha" element={<AllMatcha />} />
+          <Route path="room" element={<Room />} />
+        </Route>
+      </Routes>
+      <section id="footer">
+        <Footer />
+      </section>
+    </div>
+  );
+};
 
 export default Main;
