0f9451fb5e090ac13b38770af9259be49095785a
Sheyla De los Santos <delossantosshey@gmail.com>
2023-02-03 19:00:59 -0500

    Small updated to about and home pages

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <section className="home-intro">
-      <p className="home-paragraph">CLICK ON THE TRIANGLE TO LEARN MORE.</p>
+      <p className="home-paragraph">
+        WELCOME. <br /> CLICK ON THE KNOT TO LEARN MORE.
+      </p>
     </section>
   );
 };
