1229566f0c0bec37d47fbf1f43a7cb049d5104d4
Sheyla De los Santos <delossantosshey@gmail.com>
2023-03-08 19:15:42 -0500

    Added aria labels and alt to projects page

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -10,6 +10,7 @@ const Projects = () => {
   return (
     <section
       id="projects-images"
+      aria-label="hover to learn more about these projects."
       onMouseOver={(evt) => {
         showDesc(evt.target.id);
       }}
@@ -19,10 +20,18 @@ const Projects = () => {
     >
       <div id="projects-lisa">
         <Link to={'lisa-quest'}>
-          <img src={stars} className="image-project" id="lisa-container" />
+          <img
+            src={stars}
+            className="image-project"
+            id="lisa-container"
+            alt="stars illustration"
+          />
         </Link>
         {desc === 'lisa-container' ? (
-          <div id="lisa">
+          <div
+            id="lisa"
+            aria-label="Lisa Quest. Classic arcade style game. Click to see project page."
+          >
             <h2>LISA QUEST</h2>
             <p>Classic arcade style game</p>
           </div>
@@ -34,10 +43,14 @@ const Projects = () => {
             src={allMatcha}
             className="image-project"
             id="matcha-container"
+            alt="pink and green radiant"
           />
         </Link>
         {desc === 'matcha-container' ? (
-          <div id="matcha">
+          <div
+            id="matcha"
+            aria-label="All matcha. Fictional e-commerce website. Click to see project page."
+          >
             <h2>ALL MATCHA</h2>
             <p>Fictional e-commerce website</p>
           </div>
@@ -45,10 +58,18 @@ const Projects = () => {
       </div>
       <div>
         <Link to={'room'}>
-          <img src={room} className="image-project" id="room-container" />
+          <img
+            src={room}
+            className="image-project"
+            id="room-container"
+            alt="globe with texture on a black background"
+          />
         </Link>
         {desc === 'room-container' ? (
-          <div id="room">
+          <div
+            id="room"
+            aria-label="Room. Experiment with Three.js. Click to see project page."
+          >
             <h2>ROOM</h2>
             <p>Experiment with Three.js</p>
           </div>
