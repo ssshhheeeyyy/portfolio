6f342f951174f9048a80f3058e8c10480f3c63ee
Sheyla De los Santos <delossantosshey@gmail.com>
2023-03-09 20:26:20 -0500

    Improved footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,15 +3,32 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <a href="https://www.linkedin.com/in/sheyladelossantos/" target="_blank">
-        linkedin
-      </a>
-      <a href="https://github.com/ssshhheeeyyy" target="_blank">
-        github
-      </a>
-      <a href="mailto:delossantosshey@gmail.com" target="_blank">
-        contact
-      </a>
+      <div id="footer-links" aria-label="LinkedIn, Github and Contact links">
+        <a
+          href="https://www.linkedin.com/in/sheyladelossantos/"
+          target="_blank"
+          aria-label="Connect with me on LinkedIn"
+        >
+          linkedin
+        </a>
+        <a
+          href="https://github.com/ssshhheeeyyy"
+          target="_blank"
+          aria-label="Visit my profile on github."
+        >
+          github
+        </a>
+        <a
+          href="mailto:delossantosshey@gmail.com"
+          target="_blank"
+          aria-label="Click to send me an email."
+        >
+          contact
+        </a>
+      </div>
+      <p id="copyright">
+        Copyright 2023 © Sheyla De los Santos. Made with ♡ in NYC.
+      </p>
     </footer>
   );
 };
